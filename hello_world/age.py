fde01af9b17af1884660b213f1311463a62a86b5
Joven Bico <JovenBico@gmail.com>
2024-09-01 05:59:01 +0000

    feat: Add age.py script to input and display user's name, birthday, and age

---
+++ hello_world/age.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.11
+
+# input name, birthday, and age
+name = input("What is your name? ")
+birthday = input("When is your birthday? ")
+age = int(input("How old are you? "))
+
+# print name, birthday, and age
+print(f"Hello, {name}!")
+print(f"Your birthday is {birthday}.")
+print(f"You are {age} years old.")
