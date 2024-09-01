3e47285055e436371327de4c98988b77580c455f
Joven Bico <JovenBico@gmail.com>
2024-09-01 06:57:48 +0000

    Refactor timer.py to use local imports and improve time formatting

--- hello_to_python/timer.py
+++ hello_to_python/timer.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
-import time
+# import time
+from time import localtime, mktime, strftime
 
-start_time = time.localtime()
-print(f"Start time: {time.strftime('%X', start_time)}")
+start_time = localtime()
+print(f"Start time: {strftime('%X', start_time)}")
 
 # prest Enther to stop the timer
 input("Press 'Enter' to stop the timer")
 
-end_time = time.localtime()
+end_time = localtime()
 
 # Calculate the time taken
-time_taken = time.mktime(end_time) - time.mktime(start_time)
+time_taken = mktime(end_time) - mktime(start_time)
 
 # Print end time
-print(f"End time: {time.strftime('%X', end_time)}")
+print(f"End time: {strftime('%X', end_time)}")
 # Print the time taken
 print(f"Time taken: {time_taken} seconds")
